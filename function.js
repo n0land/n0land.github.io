7694fb36ed1741b25d6ed38cba0acc0ae2469fa7
n0land <86662707+n0land@users.noreply.github.com>
2021-10-06 12:49:22 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,42 +1,39 @@
-
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-const initialize = () => {
-  //You will start here
- window.addEventListener('DOMContentLoaded', initialize);
- const onboardButton = document.getElementById('connectButton');
-  //Created check function to see if the MetaMask extension is installed
-  const isMetaMaskInstalled = () => {
-    //Have to check the ethereum binding on the window object to see if it's installed
-    const { ethereum } = window;
-    return Boolean(ethereum && ethereum.isMetaMask);
+window.addEventListener('DOMContentLoaded', () => {
+  const onboarding = new MetaMaskOnboarding();
+  const onboardButton = document.getElementById('onboard');
+  let accounts;
+
+  const updateButton = () => {
+    if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
+      onboardButton.innerText = 'Click here to install MetaMask!';
+      onboardButton.onclick = () => {
+        onboardButton.innerText = 'Onboarding in progress';
+        onboardButton.disabled = true;
+        onboarding.startOnboarding();
+      };
+    } else if (accounts && accounts.length > 0) {
+      onboardButton.innerText = 'Connected';
+      onboardButton.disabled = true;
+      onboarding.stopOnboarding();
+    } else {
+      onboardButton.innerText = 'Connect';
+      onboardButton.onclick = async () => {
+        await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+      };
+    }
   };
-  
-    //------Inserted Code------\\
-const MetaMaskClientCheck = () => {
-  //Now we check to see if Metmask is installed
-  if (!isMetaMaskInstalled()) {
-    //If it isn't installed we ask the user to click to install it
-    onboardButton.innerText = 'Click here to install MetaMask!';
-    //When the button is clicked we call th is function
-    onboardButton.onclick = onClickInstall;
-    //The button is now disabled
-    onboardButton.disabled = false;
-  } else {
-    //If MetaMask is installed we ask the user to connect to their wallet
-    onboardButton.innerText = 'Connect';
-    //When the button is clicked we call this function to connect the users MetaMask Wallet
-    onboardButton.onclick = onClickConnect;
-    //The button is now disabled
-    onboardButton.disabled = false;
+
+  updateButton();
+  if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+    window.ethereum.on('accountsChanged', (newAccounts) => {
+      accounts = newAccounts;
+      updateButton();
+    });
   }
-};
-MetaMaskClientCheck();
-
-  //------/Inserted Code------\\
-};
-
-
-
+});
