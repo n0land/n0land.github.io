68bb9e3769668591db3dcf3693a98ae39dca5354
n0land <86662707+n0land@users.noreply.github.com>
2021-10-06 11:54:49 +0200

    Update function.js

--- function.js
+++ function.js
@@ -2,3 +2,30 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+
+const initialize = () => {
+  //Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  //------Inserted Code------\\
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if MetaMask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = 'Click here to install MetaMask!';
+    } else {
+      //If it is installed we change our button text
+      onboardButton.innerText = 'Connect';
+    }
+  };
+  MetaMaskClientCheck();
+  //------/Inserted Code------\\
+};
