56b4398b85cc6511f07ec412f78ff2f9de4237fe
n0land <86662707+n0land@users.noreply.github.com>
2021-10-06 12:02:46 +0200

    Update function.js

--- function.js
+++ function.js
@@ -3,6 +3,10 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
+const initialize = () => {
+  //You will start here
+};
+window.addEventListener('DOMContentLoaded', initialize);
 
 const initialize = () => {
   //Basic Actions Section
